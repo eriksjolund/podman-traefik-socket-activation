e43fbf74fbb1df6da814422289c360eb1ff1dc58
Erik Sjölund <erik.sjolund@gmail.com>
2025-07-14 08:47:43 +0200

    example2: Remove quic-go workaround
    Remove workaround for github issue
    https://github.com/traefik/traefik/issues/11805
    because traefik 3.4.3 fixes the issue.
    
    See also
    https://github.com/traefik/traefik/releases/tag/v3.4.3
    
    Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

--- examples/example2/prepare-images.bash
+++ examples/example2/prepare-images.bash
@@ -6,12 +6,13 @@ set -o nounset
 imagedir=$1
 
 podman build -t curl -f Containerfile.curl ./curl/
-
 podman save --format oci-archive -o "$imagedir/curl.oci" localhost/curl
 
 podman pull docker.io/traefik/whoami
-
 podman save --format oci-archive -o "$imagedir/whoami.oci" docker.io/traefik/whoami
+
+podman pull docker.io/library/traefik
+podman save --format oci-archive -o "$imagedir/traefik.oci" docker.io/library/traefik
 
 chmod -R 755 "$imagedir"
 
